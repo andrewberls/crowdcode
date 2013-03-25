6ad2729268bd30d545662b0dde8ea5de12902c3a
Petesta <Petesta@live.com>
2013-03-25 15:09:50 -0700

    Only created skeleton for the reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
