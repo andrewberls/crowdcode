04c063d408cb9a07d7d8f39aee546e3e3f3d4f0f
Andrew Berls <andrew.berls@gmail.com>
2013-04-07 14:03:23 -0700

    Review teasers

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,24 @@
 module ReviewsHelper
+
+  CODE_START = '<pre>'
+  CODE_END   = '</pre>'
+
+  def teaser(review)
+    teaser = review.body[0..100]
+
+    if start_idx = teaser.index(CODE_START)
+      if end_idx = teaser.index(CODE_END)
+        # Code block - slice it out and compact whitespace
+        end_idx += CODE_END.length # Need to go to the end of the tag
+        block   = teaser[start_idx..end_idx]
+        teaser.gsub!(block, '')
+      else
+        # Dangling start - erase everything after
+        teaser.slice!(start_idx..teaser.length)
+      end
+    end
+
+    strip_tags(teaser).squish
+  end
+
 end
